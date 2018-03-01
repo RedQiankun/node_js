d73142b7e71a21894fb820371a8cdc054f399839
RedQiankun <2669801278@qq.com>
2018-03-01 10:22:43 +0800

    node_module

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 /**
  * Created by web-01 on 2018/3/1.
  */
+const express=require('express');
+const mysql=require('mysql');
+let app=new express();
+app.use(express.static(__dirname+'/public'));
+// app.get('*',(req,res,next)=>{
+//     res.header('Access-Control-Allow-Origin','*');
+//     res.header('Access-Control-Allow-methods','PUT,POST,GET,DELETE,OPTIONS');
+//     res.header('Content-type','application/json');
+//     next();
+// });
+app.get('/demo01',(req,res)=>{
+    res.send('this is not 跨域')
+});
+app.listen(4400)
